6882b23a4f9f33c196e07e45695241e50a9d6b0a
MathFreitas <matheus.freitas_santos@outlook.com>
2021-11-10 21:48:20 -0300

    criado teste de evento "click"

--- src/components/PokemonCard/__test__/PokemonCard.test.js
+++ src/components/PokemonCard/__test__/PokemonCard.test.js
@@ -1,1 +1,27 @@
-// TODO testar component
+import React from "react";
+import { render } from "@testing-library/react";
+import PokemonCard from "../PokemonCard";
+import { cardBuilder } from "../../../__mocks__/card-builder";
+
+const setup = ({card, onClick}) => {
+    const defaultCard = card || cardBuilder(); 
+
+    const renderResult = render(<PokemonCard {...defaultCard} onClick={onClick} />);
+
+    return {
+        ...renderResult,
+        defaultCard,
+        image: renderResult.getByAltText(`${defaultCard.id}-${defaultCard.name}`)
+    }
+};
+
+describe('PokemonCard', () => {
+    test('should render with default props', () => {
+        const {container, image} = setup({});
+
+        expect(container).toBeInTheDocument();
+        expect(image).toBeInTheDocument();
+    });
+});
+
+
