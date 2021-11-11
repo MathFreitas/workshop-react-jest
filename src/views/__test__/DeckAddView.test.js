81a7a8ebf0366cc86b4fd3841bb837000cfba49c
MathFreitas <matheus.freitas_santos@outlook.com>
2021-11-11 02:18:56 -0300

    finalizando testes usando redux

--- src/views/__test__/DeckAddView.test.js
+++ src/views/__test__/DeckAddView.test.js
@@ -1,13 +1,17 @@
 import React from "react";
-import {render, wait} from "@testing-library/react";
+import {render, wait, fireEvent} from "@testing-library/react";
 import { MemoryRouter, Route } from "react-router";
 import { Provider } from "react-redux";
 import axiosMock from "../../__mocks__/axios-mock";
-
 import DeckAddView from "../DeckAddView";
 import { storeBuilder } from "../../__mocks__/store-builder";
+import {pikachuMock, squirtleMock } from "../../__mocks__/card-builder"
+
+jest.useFakeTimers();
 
 const setup = (props = {}) => {
+    jest.clearAllMocks();
+
     const store = storeBuilder();
 
     const renderResult = render(
@@ -47,7 +51,38 @@ describe('DeckAddView', () => {
         expect(container).toBeInTheDocument();
         expect(input).toBeInTheDocument();
         expect(btnAdd).toBeInTheDocument();
-        expect(queryByText(emptyMessage)).toBeInTheDocument();
-    
+        expect(queryByText(emptyMessage)).toBeInTheDocument();   
+    });
+
+    test('should render cards', async () => {
+        const cards = [pikachuMock, squirtleMock];
+
+        mockCardsResponse(cards);
+
+        const {getByAltText} = setup();
+
+        await wait(undefined, {timeout: 0});
+
+        cards.forEach((card) => {
+            expect(getByAltText(`${card.id}-${card.name}`)).toBeInTheDocument();
+        });
+    });
+
+    test('should render loading', async () => {
+        const {getByAltText} = setup();
+
+        expect(getByAltText('Pokeball Loading')).toBeInTheDocument();
+    });
+
+    test('should search', async () => {
+        const query = 'picles';
+        const {input} = setup();
+
+        fireEvent.change(input, {target: {value: query}});
+
+        jest.runAllTimers();
+
+        expect(axiosMock.get).toHaveBeenCalledTimes(2);
+        expect(axiosMock.get).toHaveBeenCalledWith(`/cards?page=1&name=${query}&pageSize=27`);
     });
 });
