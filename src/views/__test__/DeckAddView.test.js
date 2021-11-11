ddbf4182c034a9147b57de76d51c249a1014ca2d
MathFreitas <matheus.freitas_santos@outlook.com>
2021-11-11 01:28:15 -0300

    teste de renderização default utilizando a store

--- src/views/__test__/DeckAddView.test.js
+++ src/views/__test__/DeckAddView.test.js
@@ -1,1 +1,53 @@
-// TODO testar component
+import React from "react";
+import {render, wait} from "@testing-library/react";
+import { MemoryRouter, Route } from "react-router";
+import { Provider } from "react-redux";
+import axiosMock from "../../__mocks__/axios-mock";
+
+import DeckAddView from "../DeckAddView";
+import { storeBuilder } from "../../__mocks__/store-builder";
+
+const setup = (props = {}) => {
+    const store = storeBuilder();
+
+    const renderResult = render(
+        <Provider store={store}>
+            <MemoryRouter>
+                <Route path={'/'} component={DeckAddView}/>
+            </MemoryRouter>
+        </Provider>
+
+    );
+
+    return {
+        ...renderResult,
+        store,
+        emptyMessage: 'Oops... Não encontramos nada.',
+        input: renderResult.getByPlaceholderText('Pesquise...'),
+        btnAdd: renderResult.getByText('Salvar Baralho')
+    };
+};
+
+const mockCardsResponse = (cards) => {
+    axiosMock.get.mockResolvedValue({
+        data: {
+            cards
+        }
+    })
+};
+
+describe('DeckAddView', () => {
+    test('should render with default props', async () => {
+        mockCardsResponse();
+
+        const {container, queryByText, emptyMessage, input, btnAdd} = setup();
+
+        await wait(undefined,{timeout: 0});
+
+        expect(container).toBeInTheDocument();
+        expect(input).toBeInTheDocument();
+        expect(btnAdd).toBeInTheDocument();
+        expect(queryByText(emptyMessage)).toBeInTheDocument();
+    
+    });
+});
